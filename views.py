12b31181d31a7400a96254668aa2af49f50aa5d1
mrmultimediator <mrmultimediator@gmail.com>
2025-09-29 12:53:11 +0000

    Now I can list the running tasks in the UI

--- views.py
+++ views.py
@@ -1,41 +1,47 @@
-from tasks import long_running_task #-Line 1
-from celery.result import AsyncResult#-Line 2
-from flask import request, jsonify, Blueprint, render_template 
+from tasks import long_running_task
+from celery.result import AsyncResult
+from flask import request, jsonify, Blueprint, render_template, current_app
 
 views = Blueprint("views", __name__)
 
 @views.route("/", methods=["GET", "POST"])
 def home():
-    #if request.method == "POST":
-
     return render_template("home.html")
 
 @views.route("/trigger_task", methods=["POST"])
 def start_task() -> dict[str, object]:
     iterations = request.form.get('iterations')
-    print(f"Received {type(iterations)}")
-    print(iterations)
-    return "success"
-    result = long_running_task.delay(int(iterations))#-Line 3
+    result = long_running_task.delay(int(iterations))
+    # Use the Redis client from the Celery app
+    current_app.extensions["celery"].backend.client.lpush("task_ids", result.id)
     return {"result_id": result.id}
 
 @views.route("/get_result", methods=["GET"])
 def task_result() -> dict[str, object]:
     result_id = request.args.get('result_id')
-    result = AsyncResult(result_id)#-Line 4
-    if result.ready():#-Line 5
-        # Task has completed
-        if result.successful():#-Line 6
-    
+    result = AsyncResult(result_id)
+    if result.ready():
+        if result.successful():
             return {
                 "ready": result.ready(),
                 "successful": result.successful(),
-                "value": result.result,#-Line 7
+                "value": result.result,
             }
         else:
-        # Task completed with an error
             return jsonify({'status': 'ERROR', 'error_message': str(result.result)})
     else:
-        # Task is still pending
         return jsonify({'status': 'Running'})
+
+@views.route("/tasks", methods=["GET"])
+def get_tasks():
+    # Use the Redis client from the Celery app
+    task_ids_bytes = current_app.extensions["celery"].backend.client.lrange("task_ids", 0, -1)
+    task_ids = [task_id.decode('utf-8') for task_id in task_ids_bytes]
+    return jsonify(task_ids)
+
+@views.route("/tasks_list", methods=["GET"])
+def tasks_list():
+    task_ids_bytes = current_app.extensions["celery"].backend.client.lrange("task_ids", 0, -1)
+    task_ids = [task_id.decode('utf-8') for task_id in task_ids_bytes]
+    return render_template("tasks.html", task_ids=task_ids)
 
